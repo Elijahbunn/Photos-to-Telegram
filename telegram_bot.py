ce89fb6b9f5009d78dc35b2ac2695d31a9815318
Elijahbunn <eligahbunn@gmail.com>
2023-08-19 00:57:49 +0300

    publish photo to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,9 @@
+import telegram
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+BOT_TOKEN = os.environ['BOT_TOKEN']
+bot = telegram.Bot(token=BOT_TOKEN)
+bot.send_document(chat_id=['CHAT_ID'], document=open('images/epic_photo_0.png', 'rb'))
