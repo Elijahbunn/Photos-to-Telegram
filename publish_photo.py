4c1b86f9494e1686cb3d98ab737c92f2e14a6a50
Elijahbunn <eligahbunn@gmail.com>
2023-08-19 01:05:36 +0300

    Добавлены новый файл для работы с ботом и обновлен старый файл

---
+++ publish_photo.py
@@ -0,0 +1,28 @@
+import telegram
+from dotenv import load_dotenv
+import os
+import random
+import argparse
+
+from supporting_file import DIRECTORY, collecting_files, send_file
+
+
+def send_photo(photo):
+    if not photo:
+        files = collecting_files(DIRECTORY)
+        photo = random.choice(files)
+    send_file(os.path.join(DIRECTORY, photo), bot, tg_chat_id)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    tg_token = os.environ['TG_TOKEN']
+    tg_chat_id = os.environ['CHAT_ID']
+    bot = telegram.Bot(token=tg_token)
+    parser = argparse.ArgumentParser(
+        description='Программа отправляет фотографию из папки images'
+        )
+    parser.add_argument('-p', '--photo', help='Фото, которое нужно отправить',
+                        default='')
+    args = parser.parse_args()
+    send_photo(args.photo)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,34 @@
 import telegram
 from dotenv import load_dotenv
 import os
+import random
+import time
+import argparse
+
+from supporting_file import DIRECTORY, collecting_files, send_file
 
 
-load_dotenv()
-BOT_TOKEN = os.environ['BOT_TOKEN']
-bot = telegram.Bot(token=BOT_TOKEN)
-bot.send_document(chat_id=['CHAT_ID'], document=open('images/epic_photo_0.png', 'rb'))
+def send_photos(time_delay):
+    files = collecting_files(DIRECTORY)
+    while True:
+        try:
+            for image in files:
+                send_file(os.path.join(DIRECTORY, image), bot, tg_chat_id)
+                time.sleep(time_delay)
+            random.shuffle(files)
+        except telegram.error.NetworkError:
+            time.sleep(2)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    tg_token = os.environ['TG_TOKEN']
+    tg_chat_id = os.environ['CHAT_ID']
+    bot = telegram.Bot(token=tg_token)
+    parser = argparse.ArgumentParser(
+        description='Программа отправляет фотографии из папки images'
+        )
+    parser.add_argument('-d', '--delay', help='Время ожидания',
+                        default=3600, type=int)
+    args = parser.parse_args()
+    send_photos(args.delay)
