786c1385fedf29f5c5bcf19461979f91359bb21c
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2023-08-20 15:24:05 +0300

    Убрал лишние скобки в Update supporting_file.py

--- supporting_file.py
+++ supporting_file.py
@@ -10,7 +10,7 @@ def set_reading_extension(file_url):
     encoded_string = urllib.parse.unquote(file_url)
     url_parts = urllib.parse.urlsplit(encoded_string)
     path = url_parts.path
-    (file_path, file_extension) = os.path.splitext(path)
+    file_path, file_extension = os.path.splitext(path)
     return file_extension
 
 
